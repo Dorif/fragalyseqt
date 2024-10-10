7dd85ee9533140086d8a102bc778ccb594e0f71c
Alexandr Dorif <dorif11@gmail.com>
2024-10-10 20:45:12 +0300

    Minor improvements

--- setvar.py
+++ setvar.py
@@ -1,3 +1,18 @@
+# This file is part of FragalyseQt.
+#
+# FragalyseQt is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# FragalyseQt is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with FragalyseQt. If not, see <https://www.gnu.org/licenses/>.
+
 def set_graph_name(fdata):
     from charset_normalizer import from_bytes
     size_standard = "Unknown size standard, "
